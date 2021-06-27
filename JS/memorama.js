5bada72758194d7da80663666a5a4d13f9d3046a
Daniel Sebastian Jorge <daniel.jorge96@outlook.com>
2021-06-27 19:10:14 -0300

    Funcion mostrarCartas terminada

--- JS/memorama.js
+++ JS/memorama.js
@@ -1,9 +1,25 @@
 const d = document;
 
-const iniciarlizarJuego = () => {
+const mostrarCartas = () => {
+  const tarjetas = d.querySelectorAll(".area-tarjeta");
+
+  tarjetas.forEach((e) => {
+    e.style.transform = "rotatey(180deg)"
+  });
+
+  setTimeout(() => {
+    tarjetas.forEach((e) => {
+      e.style.transform = "rotatey(0deg)"
+    });
+  }, 3000);
+}
+
+const mezclarCartas = () => {
 
 }
 
 d.addEventListener("click", (e) => {
-
+  if (e.target.matches("#btn-comenzar-juego")) {
+    mostrarCartas();
+  }
 });
